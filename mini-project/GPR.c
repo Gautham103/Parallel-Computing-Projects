a6bbed6eb4642db1108fd8018c6ef52eceee94aa
Gautham <gauthamsrinivas103@gmail.com>
2020-03-20 17:28:34 -0500

    Update

--- mini-project/GPR.c
+++ mini-project/GPR.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <omp.h>
 
-#define NOISE_PARAMETER 0.01
+#define NOISE 0.01
 #define GET_RAND ((double)rand()/(10.0 * (double)RAND_MAX)) - 0.05
-#define  SIZE 25
-typedef double VEC[SIZE];
+#define  SIZE 10000
+typedef double VECTOR[SIZE];
 
 typedef struct node {
     double x;
@@ -26,6 +27,7 @@ double** alloc_mem_matrix (int n)
 
 double** multiply_matrix (double** product, double** matrix1, double** matrix2, int r1, int c1, int r2, int c2)
 {
+#pragma omp for
     for(int i = 0; i < r1; i++)
     {
         for(int j = 0; j < c2; j++)
@@ -83,6 +85,7 @@ void print_data (double *data_points, int n)
 void initialize_points (node_coord *grid_points, int m, double h)
 {
     int id = 0;
+#pragma omp for
     for(int i = 1; i <= m; i++)
     {
         for(int j = 1; j <= m; j++)
@@ -99,7 +102,7 @@ void calculate_observed_data (double **observed_data_points, node_coord *grid_po
     for(int id = 0; id < n; id++)
     {
         observed_data_points[0][id] = 1 - (((grid_points[id].x - 0.5) * (grid_points[id].x - 0.5)) +
-                       ((grid_points[id].y - 0.5) * (grid_points[id].y - 0.5)));
+                       ((grid_points[id].y - 0.5) * (grid_points[id].y - 0.5))) + GET_RAND;
     }
 
 }
@@ -115,7 +118,7 @@ void compute_K_matrix (double **K, node_coord *grid_points, int n)
             temp_y = (grid_points[i].y - grid_points[j].y) * (grid_points[i].y - grid_points[j].y);
             K[i][j] = exp(-1 * (temp_x + temp_y));
             if (i == j)
-                K[i][j] += 0.01;
+                K[i][j] += NOISE;
         }
     }
 }
@@ -132,78 +135,115 @@ void compute_k (double **k, node_coord *rstar, node_coord *grid_points, int n)
     }
 
 }
-void choldc1(int n, double ** a, VEC p)
+void cholesky_compute(int n, double ** matrix, VECTOR p)
 {
     int i,j,k;
     double sum;
 
-    for (i = 0; i < n; i++) {
-        for (j = i; j < n; j++) {
-            sum = a[i][j];
-            for (k = i - 1; k >= 0; k--) {
-                sum -= a[i][k] * a[j][k];
+#pragma omp for
+    for (i = 0; i < n; i++)
+    {
+        for (j = i; j < n; j++)
+        {
+            sum = matrix[i][j];
+            for (k = i - 1; k >= 0; k--)
+            {
+                sum -= matrix[i][k] * matrix[j][k];
             }
-            if (i == j) {
-                if (sum <= 0) {
+            if (i == j)
+            {
+                if (sum <= 0)
+                {
                     printf(" a is not positive definite!\n");
                 }
                 p[i] = sqrt(sum);
             }
-            else {
-                a[j][i] = sum / p[i];
+            else
+            {
+                matrix[j][i] = sum / p[i];
             }
         }
     }
 }
 
 
-void choldcsl(int n, double ** A, double ** a)
-{
-    int i,j,k; double sum;
-    VEC p;
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
-            a[i][j] = A[i][j];
-    choldc1(n, a, p);
-    for (i = 0; i < n; i++) {
-        a[i][i] = 1 / p[i];
-        for (j = i + 1; j < n; j++) {
-            sum = 0;
-            for (k = i; k < j; k++) {
-                sum -= a[j][k] * a[k][i];
-            }
-            a[j][i] = sum / p[j];
-        }
-    }
-}
-
-void cholsl(int n, double ** A, double ** a)
+void decompose_and_get_inverse (int n, double ** input_matrix, double ** output_matrix)
 {
     int i,j,k;
-    choldcsl(n,A,a);
-    for (i = 0; i < n; i++) {
-        for (j = i + 1; j < n; j++) {
-            a[i][j] = 0.0;
-        }
-    }
-    for (i = 0; i < n; i++) {
-        a[i][i] *= a[i][i];
-        for (k = i + 1; k < n; k++) {
-            a[i][i] += a[k][i] * a[k][i];
-        }
-        for (j = i + 1; j < n; j++) {
-            for (k = j; k < n; k++) {
-                a[i][j] += a[k][i] * a[k][j];
+    double sum;
+    VECTOR p;
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            output_matrix[i][j] = input_matrix[i][j];
+
+    cholesky_compute (n, output_matrix, p);
+
+#pragma omp for
+    for (i = 0; i < n; i++)
+    {
+        output_matrix[i][i] = 1 / p[i];
+        for (j = i + 1; j < n; j++)
+        {
+            sum = 0;
+            for (k = i; k < j; k++)
+            {
+                sum -= output_matrix[j][k] * output_matrix[k][i];
             }
-        }
-    }
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < i; j++) {
-            a[i][j] = a[j][i];
+            output_matrix[j][i] = sum / p[j];
         }
     }
 }
 
+void get_inverse_by_cholesky (int n, double ** input_matrix, double ** output_matrix)
+{
+    int i,j,k;
+    decompose_and_get_inverse (n, input_matrix, output_matrix);
+#pragma omp for
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            output_matrix[i][j] = 0.0;
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        output_matrix[i][i] *= output_matrix[i][i];
+        for (k = i + 1; k < n; k++)
+        {
+            output_matrix[i][i] += output_matrix[k][i] * output_matrix[k][i];
+        }
+        for (j = i + 1; j < n; j++)
+        {
+            for (k = j; k < n; k++)
+            {
+                output_matrix[i][j] += output_matrix[k][i] * output_matrix[k][j];
+            }
+        }
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            output_matrix[i][j] = output_matrix[j][i];
+        }
+    }
+}
+
+double run_solver (double **K_inverse, double **k, double **observed_data_points, int n)
+{
+    double **product = alloc_mem_matrix (n);
+    double **output = alloc_mem_matrix (n);
+#pragma omp task default (none) shared(product, K_inverse, k, n)
+    multiply_matrix (product, K_inverse, k, n , n, n, 1);
+
+#pragma omp task default (none) shared(output, observed_data_points, product, n)
+    multiply_matrix (output, observed_data_points, product, 1, n, n, 1);
+
+    return output[0][0];
+
+}
 
 int main(int argc, char* argv[])
 {
@@ -230,32 +270,36 @@ int main(int argc, char* argv[])
     h = 1.0 / (double)(m + 1);
 
     node_coord *grid_points = (node_coord *) malloc (n * sizeof (node_coord));
+#pragma omp task default (none) shared(grid_points, m, h)
     initialize_points (grid_points, m, h);
-    print_points (grid_points, m);
+#pragma omp taskwait
 
     double **observed_data_points = alloc_mem_matrix (n);
     calculate_observed_data (observed_data_points, grid_points, n);
-    print_matrix ("observed_data", observed_data_points, n);
 
     double **K = alloc_mem_matrix (n);
     compute_K_matrix (K, grid_points, n);
-    print_matrix ("K matrix", K, n);
 
     double **k = alloc_mem_matrix (n);
     compute_k (k, rstar, grid_points, n);
-    print_matrix ("k vector", k, n);
 
     double **K_inverse = alloc_mem_matrix (n);
-    cholsl(n, K, K_inverse);
-    print_matrix ("k inverse", K_inverse, n);
+    double start_time = omp_get_wtime();
+#pragma omp task default (none) shared(n, K, K_inverse)
+    get_inverse_by_cholesky(n, K, K_inverse);
+#pragma omp taskwait
+    double end_time = omp_get_wtime();
+    printf ("Time taken by cholesky = %10.5e\n", end_time - start_time);
 
-    double **product = alloc_mem_matrix (n);
-    multiply_matrix (product, K_inverse, k, n , n, n, 1);
-    print_matrix ("product of K_inverse and k vector", product, n);
+    start_time = omp_get_wtime();
+    double predicted_value = run_solver (K_inverse, k, observed_data_points, n);
+    end_time = omp_get_wtime();
 
-    double **output = alloc_mem_matrix (n);
-    multiply_matrix (output, observed_data_points, product, 1, n, n, 1);
-    printf ("Predicted value is %lf\n", output[0][0]);
+    double actual_value = 1 - (((rstar[0].x - 0.5) * (rstar[0].x - 0.5)) +
+                                    ((rstar[0].y - 0.5) * (rstar[0].y - 0.5))) + GET_RAND;
+    printf ("Time taken by solver = %10.5e\n", end_time - start_time);
+    printf ("Actual value is %lf\n", actual_value);
+    printf ("Predicted value is %lf\n", predicted_value);
+
     return 0;
-
 }
